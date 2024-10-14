9ef7eabdfba0f18f4a6c94d6d3dbb2c74367b3d9
Kai <139108608+NghiaNguyen04@users.noreply.github.com>
2024-10-14 14:48:38 +0700

    Khởi tạo kho lưu trữ với cấu trúc khoa học dữ liệu cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='du an machine learning',
+    author='Group 11',
+    license='',
+)
